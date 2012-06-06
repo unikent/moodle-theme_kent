d48aa88d9b101e8000a06a4331af66b3ca53067e
unknown <fg30@kent.ac.uk>
2012-06-06 15:40:18 +0100

    Added in hiding of docking/edit options for blocks
    Admins will still see options, all other users will have particular blocks hiding the options.

--- renderers.php
+++ renderers.php
@@ -14,51 +14,57 @@
  * This is left as comments as an example of how to get set up.
  */
 
-//class theme_kent_core_renderer extends core_renderer {
-//
-//    protected function block_header(block_contents $bc) {
-//
-//        $show_block_controls = self::kent_determine_show_block_controls($bc->attributes['class'], $bc->controls);
-//
-//        $title = '';
-//        if ($bc->title) {
-//            $title = html_writer::tag('h2', $bc->title, null);
-//        }
-//
-//        $controlshtml = '';
-//        if ($show_block_controls){
-//            $controlshtml = $this->block_controls($bc->controls);
-//        }
-//
-//        $output = '';
-//        if ($title || $controlshtml) {
-//            $output .= html_writer::tag('div', html_writer::tag('div', html_writer::tag('div', '', array('class'=>'block_action')). $title . $controlshtml, array('class' => 'title')), array('class' => 'header'));
-//        }
-//        return $output;
-//    }
-//
-//    private function kent_determine_show_block_controls($class, $controls){
-//        //First check if not an admin and there are controls before doing anything
-//        //TODO - do a more sophisticated permission check or role check
-//        $admin = FALSE;
-//        if(!$admin && !empty($controls)){
-//
-//            //TODO - Move this somewhere else in a more controllable area so FLT's can control what blocks cannot be controlled by what role etc.
-//            $restrict_blocks = array('kent_rollover', 'recent_activity');
-//
-//            foreach($restrict_blocks as $restricted_block){
-//                $check_block = "block_$restricted_block";
-//
-//                if (strstr($class, $check_block)){
-//                    return false;
-//                }
-//            }
-//        }
-//
-//        return true;
-//
-//    }
-//
-//}
+class theme_kent_core_renderer extends core_renderer {
+
+    protected function block_header(block_contents $bc) {
+
+        $show_block_controls = self::kent_determine_show_block_controls($bc->attributes['class'], $bc->controls);
+
+        $title = '';
+        if ($bc->title) {
+            $title = html_writer::tag('h2', $bc->title, null);
+        }
+
+        $controlshtml = '';
+        $headerclass = 'header hidedock';
+        if ($show_block_controls){
+            $controlshtml = $this->block_controls($bc->controls);
+            $headerclass = 'header';
+        } else {
+            $bc->collapsible = 0;
+        }
+
+        $output = '';
+        if ($title || $controlshtml) {
+            $output .= html_writer::tag('div', html_writer::tag('div', html_writer::tag('div', '', array('class'=>'block_action')). $title . $controlshtml, array('class' => 'title')), array('class' => $headerclass));
+        }
+
+        return $output;
+    }
+
+
+    private function kent_determine_show_block_controls($class, $controls){
+        //First check if not an admin and there are controls before doing anything
+        
+        $systemcontext = get_context_instance(CONTEXT_SYSTEM);
+        if(!has_capability('moodle/site:config', $systemcontext) && !empty($controls)){
+
+            //TODO - Move this somewhere else in a more controllable area so FLT's can control what blocks cannot be controlled by what role etc.
+            $restrict_blocks = array('kent_course_overview', 'calendar_upcoming', 'calendar_month', 'activity_modules', 'navigation');
+
+            foreach($restrict_blocks as $restricted_block){
+                $check_block = "block_$restricted_block";
+
+                if (strstr($class, $check_block)){
+                    return FALSE;
+                }
+            }
+        }
+
+        return TRUE;
+
+    }
+
+}
 
 ?>
