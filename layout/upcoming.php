d44fc5ca7d00ba6819e81dbfac70a9a1d5b2713d
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-11-27 16:35:33 +0000

    Cleanup

--- layout/upcoming.php
+++ layout/upcoming.php
@@ -1,14 +1,15 @@
 <?php
-
-global $USER, $CFG, $SESSION;
-$cal_m = optional_param( 'cal_m', 0, PARAM_INT );
-$cal_y = optional_param( 'cal_y', 0, PARAM_INT );
+/**
+ * Upcoming Events list
+ */
 
 require_once($CFG->dirroot.'/calendar/lib.php');
 
+global $CFG;
 
+$cal_m = optional_param( 'cal_m', 0, PARAM_INT );
+$cal_y = optional_param( 'cal_y', 0, PARAM_INT );
 
-$filtercourse    = array();
 if (empty($this->instance)) { // Overrides: use no course at all
     $courseshown = false;
 } else {
@@ -19,23 +20,21 @@ if (empty($this->instance)) { // Overrides: use no course at all
 // the list of courses it will be grabbing events from.
 $filtercourse = calendar_get_default_courses();
 
-
 list($courses, $group, $user) = calendar_set_filters($filtercourse);
 
 $defaultlookahead = CALENDAR_DEFAULT_UPCOMING_LOOKAHEAD;
 if (isset($CFG->calendar_lookahead)) {
     $defaultlookahead = intval($CFG->calendar_lookahead);
 }
+
 $lookahead = get_user_preferences('calendar_lookahead', $defaultlookahead);
 
 $defaultmaxevents = CALENDAR_DEFAULT_UPCOMING_MAXEVENTS;
 if (isset($CFG->calendar_maxevents)) {
     $defaultmaxevents = intval($CFG->calendar_maxevents);
 }
+
 $maxevents = get_user_preferences('calendar_maxevents', 3);
 $events = calendar_get_upcoming($courses, $group, $user, $lookahead, $maxevents);
-
       
-$upcomingoutput = calendar_get_block_upcoming($events, 'view.php?view=day&amp;course='.$courseshown.'&amp;');
-
-echo $upcomingoutput;
+echo calendar_get_block_upcoming($events, 'view.php?view=day&amp;course='.$courseshown.'&amp;');
