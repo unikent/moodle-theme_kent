3464216119d7e922afb7cc651fc2553d1e403ffd
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-11-27 10:23:51 +0000

    Fix course blocks

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013050100;
+$plugin->version   = 2013112700;
 $plugin->requires  = 2013050100;
 $plugin->component = 'theme_kent';
 $plugin->dependencies = array(
