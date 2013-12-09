cf609cdc6523467b8e5861d5611dce62161a9781
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-12-09 14:15:49 +0000

    Decrease left indent of activities text

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013120900;
+$plugin->version   = 2013120901;
 $plugin->requires  = 2013050100;
 $plugin->component = 'theme_kent';
 $plugin->dependencies = array(
