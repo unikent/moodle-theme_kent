536fc1b97f7ddccf380d424a8471540eaf6c63e8
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-15 09:20:08 +0000

    Bump theme version for asset re-delivery

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014010900;
+$plugin->version   = 2014011500;
 $plugin->requires  = 2013050100;
 $plugin->component = 'theme_kent';
 $plugin->dependencies = array(
