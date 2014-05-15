0977e4aaa62fc01b15521d1a48e22d7e03013ea8
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-05-15 16:50:00 +0100

    Update version.php

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014043000;
-$plugin->requires  = 2013051500;
+$plugin->version   = 2014051500;
+$plugin->requires  = 2013050100;
 $plugin->component = 'theme_kent';
 $plugin->dependencies = array(
     'theme_base'  => 2013050100,
