e173b9cb5f2eca7a1924c0d2588a8d51117f5ef8
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-08-22 13:04:37 +0100

    Unix line endings please.

--- config.php
+++ config.php
@@ -1,1 +1,146 @@
-<?php////////////////////////////////////////////////////// Name of the theme. Most likely the name of// the directory in which this file resides.////////////////////////////////////////////////////$THEME->name = 'kent';/////////////////////////////////////////////////////// Which existing theme(s) in the /theme/ directory// do you want this theme to extend. A theme can// extend any number of themes. Rather than// creating an entirely new theme and copying all// of the CSS, you can simply create a new theme,// extend the theme you like and just add the// changes you want to your theme.////////////////////////////////////////////////////$THEME->parents = array('base','canvas', 'aardvark_kent');////////////////////////////////////////////////////// Name of the stylesheet(s) you've including in// this theme's /styles/ directory.////////////////////////////////////////////////////if (isset($CFG->theme_colour)) {    switch($CFG->theme_colour) {        case "archive":            $THEME->sheets = array('core', 'archive');            break;        case "cyan":            $THEME->sheets = array('core', 'cyan');            break;        case "green":            $THEME->sheets = array('core', 'green');            break;        case "red":            $THEME->sheets = array('core', 'red');            break;        case "clean":            $THEME->sheets = array('core', 'clean');            break;        case "grey":            $THEME->sheets = array('core', 'grey');            break;        case "future":            $THEME->sheets = array('core', 'print', 'future');            break;        case "blue":        default:            $THEME->sheets = array('core', 'print');    }} else {    $THEME->sheets = array('core', 'print');}////////////////////////////////////////////////////// Do you want to use the new navigation dock?////////////////////////////////////////////////////$THEME->enable_dock = false;////////////////////////////////////////////////////// An array of stylesheets to include within the// body of the editor.////////////////////////////////////////////////////$THEME->editor_sheets = array('editor');$THEME->layouts = array(    'base' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post',    ),    'general' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post',    ),    'course' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post'    ),    'coursecategory' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post',    ),    'incourse' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post',    ),    'frontpage' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post',    ),    'admin' => array(        'file' => 'general.php',        'regions' => array('side-pre'),        'defaultregion' => 'side-pre',    ),    'mydashboard' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post',        'options' => array('langmenu'=>true),    ),    'mypublic' => array(        'file' => 'general.php',        'regions' => array('side-pre', 'side-post'),        'defaultregion' => 'side-post',    ),    'login' => array(        'file' => 'general.php',        'regions' => array(),        'options' => array('langmenu'=>true),    ),    'popup' => array(        'file' => 'embedded.php',        'regions' => array(),        'options' => array('nofooter'=>true, 'noblocks'=>true, 'nonavbar'=>true),    ),    'frametop' => array(        'file' => 'general.php',        'regions' => array(),        'options' => array('nofooter'=>true),    ),    'maintenance' => array(        'file' => 'general.php',        'regions' => array(),        'options' => array('nofooter'=>true, 'nonavbar'=>true),    ),    'embedded' => array(    	'theme'=>'canvas',        'file' => 'embedded.php',        'regions' => array(),        'options' => array('nofooter'=>true, 'nonavbar'=>true),    ),    // Should display the content and basic headers only.    'print' => array(        'file' => 'general.php',        'regions' => array(),        'options' => array('nofooter'=>true, 'nonavbar'=>false, 'noblocks'=>true),    ),    'report' => array(        'file' => 'general.php',        'regions' => array('side-pre'),        'defaultregion' => 'side-pre',    ),    'datool' => array(        'file' => 'general.php',        'regions' => array(),    ),);$THEME->csspostprocess = 'kent_process_css';
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$THEME->name = 'kent';
+
+$THEME->parents = array('base', 'canvas', 'aardvark_kent');
+
+if (isset($CFG->theme_colour)) {
+    switch($CFG->theme_colour) {
+        case "archive":
+            $THEME->sheets = array('core', 'archive');
+            break;
+        case "cyan":
+            $THEME->sheets = array('core', 'cyan');
+            break;
+        case "green":
+            $THEME->sheets = array('core', 'green');
+            break;
+        case "red":
+            $THEME->sheets = array('core', 'red');
+            break;
+        case "clean":
+            $THEME->sheets = array('core', 'clean');
+            break;
+        case "grey":
+            $THEME->sheets = array('core', 'grey');
+            break;
+        case "future":
+            $THEME->sheets = array('core', 'print', 'future');
+            break;
+        case "blue":
+        default:
+            $THEME->sheets = array('core', 'print');
+    }
+} else {
+    $THEME->sheets = array('core', 'print');
+}
+
+$THEME->enable_dock = false;
+
+$THEME->editor_sheets = array('editor');
+
+$THEME->layouts = array(
+    'base' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'general' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'course' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post'
+    ),
+    'coursecategory' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'incourse' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'frontpage' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'admin' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    'mydashboard' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+        'options' => array('langmenu' => true),
+    ),
+    'mypublic' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'login' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('langmenu' => true),
+    ),
+    'popup' => array(
+        'file' => 'embedded.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'noblocks' => true, 'nonavbar' => true),
+    ),
+    'frametop' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true),
+    ),
+    'maintenance' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nonavbar' => true),
+    ),
+    'embedded' => array(
+        'theme' => 'canvas',
+        'file' => 'embedded.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nonavbar' => true),
+    ),
+    // Should display the content and basic headers only.
+    'print' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nonavbar' => false, 'noblocks' => true),
+    ),
+    'report' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    'datool' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+    ),
+);
+
+$THEME->csspostprocess = 'kent_process_css';
