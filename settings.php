a6407675217b92d3fc3e40ed231a76520733e1c0
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-25 14:01:04 +0000

    Get rid of old Moodle themes

--- settings.php
+++ settings.php
@@ -1,10 +1,22 @@
 <?php
-
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
-
     // NavBar Setting.
     $settings->add(new admin_setting_configcheckbox(
         'theme_kent_enable_navbar',
@@ -12,37 +24,4 @@ if ($ADMIN->fulltree) {
         'Enable Kent Navbar',
         0
     ));
-
-// Graphic Wrap (Background Image)
-$name = 'theme_kent/graphicwrap';
-$title=get_string('graphicwrap','theme_kent');
-$description = get_string('graphicwrapdesc', 'theme_kent');
-$setting = new admin_setting_configtext($name, $title, $description, '', PARAM_URL);
-$settings->add($setting);
-
-// Logo file setting
-$name = 'theme_kent/logo';
-$title = get_string('logo','theme_kent');
-$description = get_string('logodesc', 'theme_kent');
-$setting = new admin_setting_configtext($name, $title, $description, '', PARAM_URL);
-$settings->add($setting);
-
-// Menu select background colour setting
-$name = 'theme_kent/menuhovercolor';
-$title = get_string('menuhovercolor','theme_kent');
-$description = get_string('menuhovercolordesc', 'theme_kent');
-$default = '#fd7f11';
-$previewconfig = NULL;
-$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
-$settings->add($setting);
-
-
-// Foot note setting
-$name = 'theme_kent/footnote';
-$title = get_string('footnote','theme_kent');
-$description = get_string('footnotedesc', 'theme_kent');
-$default = '';
-$setting = new admin_setting_confightmleditor($name, $title, $description, $default);
-$settings->add($setting);
-
 }
