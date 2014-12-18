f7c7a361d8de71ff0b68fa835e4cf9591c6f57ee
Skylar Kelty <skylarkelty@gmail.com>
2014-12-18 10:14:12 +0000

    Blank slate for the new homepage

--- layout/homepage.php
+++ layout/homepage.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$bodyclasses = array('kent-embedded');
+$bodyclasses = array('kent-homepage');
 
 require(dirname(__FILE__) . "/includes/header.php");
 ?>
