29954f68db4ece3e86ab622925d43f3f158c07cb
Skylar Kelty <s.kelty@kent.ac.uk>
2015-02-24 13:59:16 +0000

    Update grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,23 @@ module.exports = function(grunt) {
     require('jit-grunt')(grunt);
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         less: {
             development: {
                 options: {
                     compress: true,
                     yuicompress: true,
-                    optimization: 2
+                    optimization: 2,
+                    banner: '/*! <%= pkg.description %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                    plugins: [
+                        new (require('less-plugin-autoprefix'))({
+                            browsers: ["last 2 versions"]
+                        }),
+                        new (require('less-plugin-clean-css'))({
+                            advanced: true,
+                            compatibility: 'ie8'
+                        })
+                    ]
                 },
                 files: {
                     "style/theme.css": "style/less/theme.less",
