e2468135f5512a51534a3136495c23699bc01576
Skylar Kelty <s.kelty@kent.ac.uk>
2015-03-11 11:26:23 +0000

    Override connect renderer

--- renderers.php
+++ renderers.php
@@ -20,6 +20,7 @@ if (\theme_kent\core::is_beta()) {
 	require_once('renderers/future_maintenance_renderer.php');
 	require_once('renderers/future_quiz_renderer.php');
 	require_once('renderers/future_kco_renderer.php');
+	require_once('renderers/future_connect_renderer.php');
 } else {
 	require_once('renderers/current_renderer.php');
 }
