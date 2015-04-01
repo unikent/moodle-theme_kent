c2483b9d72cffd2b2f7f0664a1e994e35e3b9610
Skylar Kelty <s.kelty@kent.ac.uk>
2015-04-01 16:13:06 +0100

    Update settings block search

--- renderers/future_block_renderers.php
+++ renderers/future_block_renderers.php
@@ -15,6 +15,45 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 require_once($CFG->dirroot . '/blocks/kent_course_overview/renderer.php');
+require_once($CFG->dirroot . '/blocks/settings/renderer.php');
+
+/**
+ * Overrides a few defaults.
+ *
+ * @package     theme_kent
+ * @copyright   2015 Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class theme_kent_block_settings_renderer extends block_settings_renderer
+{
+    public function search_form(moodle_url $formtarget, $searchvalue) {
+        $content = html_writer::start_tag('form', array('class'=>'adminsearchform', 'method'=>'get', 'action'=>$formtarget, 'role' => 'search'));
+        $content .= html_writer::start_tag('div');
+
+        $content .= html_writer::start_tag('div', array('class' => 'input-group'));
+            $content .= html_writer::empty_tag('input', array(
+                'type' => 'text',
+                'id' => 'adminsearchquery',
+                'name' => 'query',
+                'value' => s($searchvalue),
+                'class' => 'form-control',
+                'placeholder' => s(get_string('searchinsettings', 'admin'))
+            ));
+
+            $content .= html_writer::start_tag('span', array('class' => 'input-group-btn'));
+                $content .= html_writer::tag('button', '<i class="fa fa-search"></i>', array(
+                    'class' => 'btn btn-default',
+                    'type' => 'button'
+                ));
+            $content .= html_writer::end_tag('span');
+        $content .= html_writer::end_tag('div');
+
+        $content .= html_writer::end_tag('div');
+        $content .= html_writer::end_tag('form');
+        return $content;
+    }
+}
+
 
 /**
  * Overrides a few defaults.
