e65114ca1e0f2951570317cc5f60f5fd4b9a53ff
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-06-01 14:31:32 +0100

    Version bump

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015052800;
+$plugin->version   = 2015060100;
 $plugin->requires  = 2013050100;
 $plugin->component = 'theme_kent';
 $plugin->dependencies = array(
