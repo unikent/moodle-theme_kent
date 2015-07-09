3876edb25daf26e9da7cdc4398953009c768c8be
Skylar Kelty <s.kelty@kent.ac.uk>
2015-07-09 13:18:07 +0100

    Move to hjs

--- amd/src/theme.js
+++ amd/src/theme.js
@@ -71,14 +71,18 @@ define([], function() {
 
 		    $("select,textarea,input[type=text],input[type=password],input[type=datetime],input[type=datetime-local],input[type=date],input[type=month],input[type=time],input[type=week],input[type=number],input[type=email],input[type=url],input[type=search],input[type=tel],input[type=color]").addClass("form-control");
 
-            if ($('code[class*="language-"]').length > 0) {
-                $('code[class*="language-"]').each(function() {
-                    $(this).wrap("<pre class=\"" + $(this).attr('class') + "\">");
+            if ($('code').length > 0) {
+                $('code').each(function() {
+                    $(this).html($(this).html().trim()).wrap("<pre>");
                 });
 
-                $.getScript(M.cfg.wwwroot + '/theme/kent/javascript/prism.js', function() {
-                    Prism.highlightAll();
+                require(['theme_kent/hljs'], function(hljs) {
+                	window.hljs.initHighlightingOnLoad();
                 });
+
+                //$.getScript(M.cfg.wwwroot + '/theme/kent/javascript/prism.js', function() {
+                //    Prism.highlightAll();
+                //});
             }
         }
     }
