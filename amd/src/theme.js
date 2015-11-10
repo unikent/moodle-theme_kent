9b85f832f1c497d1b349ad24217967e89897cae1
Skylar Kelty <s.kelty@kent.ac.uk>
2015-11-10 11:42:29 +0000

    Upgrade hljs

--- amd/src/theme.js
+++ amd/src/theme.js
@@ -73,13 +73,13 @@ define([], function() {
 		    $("select,textarea,input[type=text],input[type=password],input[type=datetime],input[type=datetime-local],input[type=date],input[type=month],input[type=time],input[type=week],input[type=number],input[type=email],input[type=url],input[type=search],input[type=tel],input[type=color]").addClass("form-control");
 
             // Code highlighting.
-            if ($('code:not(.nohighlight)').length > 0) {
-                $('code:not(.nohighlight)').each(function() {
+            if ($('pre code:not(.nohighlight)').length > 0) {
+                $('pre code:not(.nohighlight)').each(function() {
                     //$(this).html($(this).html().trim()).wrap("<pre>");
                 });
 
                 require(['theme_kent/hljs'], function(hljs) {
-                	$('code:not(.nohighlight)').each(function(i, block) {
+                	$('pre code:not(.nohighlight)').each(function(i, block) {
 						window.hljs.highlightBlock(block);
 					});
                 });
