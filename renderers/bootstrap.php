8013b00cfb641e15e72ee79f2329947cbbc59c68
Skylar Kelty <s.kelty@kent.ac.uk>
2016-01-14 16:22:58 +0000

    Fix notifications with multiple classes

--- renderers/bootstrap.php
+++ renderers/bootstrap.php
@@ -31,37 +31,39 @@ trait theme_kent_bootstrap_notifications {
                 if ($part == 'dismissable') {
                     $dismissable = true;
                 } else {
-                    $classes[] = $part;
+                    switch ($part) {
+                        case 'notifydanger':
+                        case 'notifyerror':
+                        case 'notifyproblem':
+                        case 'notifytiny':
+                            $classes[] = 'alert';
+                            $classes[] = 'alert-danger';
+                        break;
+
+                        case 'notifysuccess':
+                            $classes[] = 'alert';
+                            $classes[] = 'alert-success';
+                        break;
+                        
+                        case 'notifywarning':
+                            $classes[] = 'alert';
+                            $classes[] = 'alert-warning';
+                        break;
+                        
+                        case 'notifyinfo':
+                        case 'notifymessage':
+                        case 'redirectmessage':
+                            $classes[] = 'alert';
+                            $classes[] = 'alert-info';
+                        break;
+
+                        default:
+                            $classes[] = $part;
+                        break;
+                    }
                 }
             }
             $classes = implode(' ', $classes);
-        }
-
-        switch ($classes) {
-            case 'notifydanger':
-            case 'notifyerror':
-            case 'notifyproblem':
-            case 'notifytiny':
-                $classes = 'alert alert-danger';
-            break;
-
-            case 'notifysuccess':
-                $classes = 'alert alert-success';
-            break;
-            
-            case 'notifywarning':
-                $classes = 'alert alert-warning';
-            break;
-            
-            case 'notifyinfo':
-            case 'notifymessage':
-            case 'redirectmessage':
-                $classes = 'alert alert-info';
-            break;
-
-            default:
-                $classes = $classes;
-            break;
         }
 
         $button = '';
